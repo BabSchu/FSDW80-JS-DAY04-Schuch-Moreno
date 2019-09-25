3aa8e4fbcb907d8e8d3b19a26a6b70b4232a3ed9
BabSchu <you@example.com>
2019-09-25 15:40:20 +0200

    add hotel exercise

---
+++ intermediate/index.js
@@ -0,0 +1,36 @@
+/*Intermediate
+Intermediate exercise 1 | Hotels
+
+Create three objects of type hotel. Each of them shares some properties and methods. 
+The user should be able to insert a number of nights for each hotel and get back results, 
+whether the hotel is available or not (for some number of nights). For each hotel generate 
+random integers from 1-10 that store available nights. If the hotel is available, 
+provide a button labeled "I'll reserve" otherwise provide a message with 
+text "Sorry, no free room for <number_of_nights> nights".
+
+Try to style the program with the colors #003580, #FFFFFF and #E9F0FA.*/
+
+var c = Math.floor((Math.random() * 10) + 1);
+
+function HotelConstructor (name) {
+
+	this.name = name;
+
+	this.mes = function (){
+		this.nights = document.getElementById("numberOfNights").value;
+		this.avaiNights = c;
+		var result = this.avaiNights - Number(this.nights);
+
+		if ( result <= 0){
+			document.getElementById("messagetext").innerHTML = "Sorry, no free room for nights";
+		}
+
+		else {
+			document.getElementById("messagetext").innerHTML = "I'll reserve";
+		}
+	}
+}
+
+var boutiqueHotel = new HotelConstructor ("Leonardo");
+
+document.getElementById("button").onclick = boutiqueHotel.mes;
